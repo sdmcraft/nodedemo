96521403ff3efba16a21d7ba66730c1370c2cd08
Satya Deep Maheshwari <satyam@adobe.com>
2019-10-22 17:22:23 +0530

    upload - wip

--- upload.js
+++ upload.js
@@ -1,12 +1,15 @@
 var request = require('request');
 var fs = require('fs');
 const formData = {
-    file: fs.createReadStream('GameMania.psd')
+    file: fs.createReadStream('/Users/satyam/Downloads/to-be-trash/GameMania.psd')
 }
 var start = new Date();
 request({
     method: 'POST',
     url: 'http://aem65.aal.corp.adobe.com:4502/content/dam/test.createasset.html',
+    headers: {
+        'Sling-uploadmode': 'stream'
+      },
     formData: formData
   },
   function (error, response, body) {
